e6e481fcdc7315243aae7b22d93a8b992e551577
zipeng <1437966391@qq.com>
2023-09-15 15:15:04 +0800

    fix: 错误测试

--- src/main/java/Util/SimHashUtils.java
+++ src/main/java/Util/SimHashUtils.java
@@ -22,8 +22,9 @@ public class SimHashUtils {
 
     //传入String,计算simHash值，以字符串形式输出
     public static String getSimHash(String str) {
-        // 文本长度太短时HanLp无法取得关键字
+        // 判断文件是否存在以及文本长度
         try {
+            if (str.length() == 0) throw new ShortStringException("文件为空");
             if (str.length() < 200) throw new ShortStringException("文本过短，难以判断！");
         } catch (ShortStringException e) {
             e.printStackTrace();
